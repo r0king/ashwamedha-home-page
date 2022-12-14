c8fb9abdf3b043aa83de9c5388815c0553aed692
Royal Babu <123royalbabu@gmail.com>
2022-12-14 18:23:20 +0530

    update hero page with logo and text change navbar to fade out and added line in the bottom

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import bgVideo from "../assests/bgvideo.webm";
 import Navbar from "./Navbar";
-
+import Logo from "../assests/logo.png";
 export default class Hero extends Component {
   constructor(props) {
     super();
@@ -9,17 +9,32 @@ export default class Hero extends Component {
   render() {
     return (
       <>
-      <Navbar/>
+        <Navbar />
         <div>
-        <video
-          width={window.innerWidth}
-          className="right-0 bottom-0 top-0 min-h-[100%] min-w-[100%]"
-          loop
-          autoPlay
-          muted
-        >
-          <source src={bgVideo} type="video/mp4" />
-        </video>
+          <video
+            width={window.innerWidth}
+            className="right-0 bottom-0 top-0 min-h-[100%] min-w-[100%] absolute -z-20"
+            loop
+            autoPlay
+            muted
+          >
+            <source src={bgVideo} type="video/mp4" />
+          </video>
+          <div
+            width={window.innerWidth}
+            className="h-[80vh] bg-black/[0.7] z-10 lg:h-[100vh]"
+          >
+            <div className="flex justify-between">
+              <img
+                src={Logo}
+                alt=""
+                className="invert w-1/2 pt-[20vh] pl-[5vw] lg:w-1/3"
+              />
+              <div className="flex flex-col justify-center">
+                  asdfasdfasdf
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
