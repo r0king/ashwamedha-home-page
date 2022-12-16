799dfb7ef92ffbbbe21220f3b466ef53c0425b21
Sid9021 <sidharthkajith9021@gmail.com>
2022-12-17 01:47:33 +0530

    final about us, modified highlights (hero page broken only masked logo)

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -8,15 +8,15 @@ export default class About extends Component {
        <section className="bg-grey-900">
         <div className="container px-6 py-10 mx-auto flex ">
           <div className="lg:-mx-6 lg:flex lg:items-center justify-center flex-auto sm:flex sm:items-center ">
-            <img className="object-cover object-center lg:w-1/3 sm:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-full sm:h-full" src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
+            <img className="object-cover object-center lg:w-1/4 sm:w-1/2 lg:mx-6 w-full h-96 rounded-lg lg:h-full sm:h-full" src="https://images.unsplash.com/photo-1499470932971-a90681ce8530?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
             <div className="lg:w-1/2 lg:px-6 lg:mt-0 sm:w-1/2 sm:ml-6">
-              <p className="text-3xl font-semibold text-justify text-orange-400"><span className="inline-block w-16 h-0.5 bg-orange-400 rounded-full mb-2 mr-12" />ABOUT US</p>
-              <p className="text-5xl font-semibold text-justify text-white"><span classname="text-5xl font-bold text-gray-500 mr-6">01</span>Who We Are</p>
+              <p className="text-3xl font-semibold text-justify text-orange-400 mt-2"><span className="inline-block w-16 h-0.5 bg-orange-400 rounded-full mb-2 mr-12" />ABOUT US</p>
+              <p className="text-5xl font-semibold text-justify text-white mt-2">Who We Are</p>
               <p className="max-w-lg mt-6 text-gray-500 dark:text-gray-400 text-justify">
                 “ Sunt nisi consequat cupidatat consequat adipisicing consequat anim proident deserunt culpa pariatur adipisicing excepteur. Irure elit ullamco eiusmod reprehenderit do non. Lorem cillum laboris amet sint minim. Amet quis exercitation in elit. Tempor culpa aute aliqua nostrud Lorem aliqua veniam do minim incididunt tempor. Irure enim deserunt labore nulla cillum incididunt reprehenderit exercitation.
                 Laboris fugiat aliquip Lorem in id consectetur culpa nisi laborum id. Id elit non officia id irure do aute consectetur aute dolor commodo dolore non. Do laboris Lorem amet eiusmod enim dolor ut officia nulla Lorem quis ex nisi ex. Amet labore amet pariatur proident tempor nostrud non nostrud ipsum quis culpa dolor fugiat nostrud. Aliquip minim sunt sunt esse esse. Consectetur voluptate do eiusmod nostrud non deserunt amet aliquip cupidatat sit.
               </p>
-              <a href='#'><p className='text-2xl font-semibold'><span className="inline-block w-0.5 h-5 bg-orange-400 rounded-full mt-4 mr-4" />READ MORE<span className="inline-block w-0.5 h-5 bg-orange-400 rounded-full mt-4 ml-4"/></p></a>
+              <a href='#'><p className='text-xl font-semibold'><span className="inline-block w-0.5 h-4 bg-orange-400 rounded-full mt-4 mr-4" />READ MORE<span className="inline-block w-0.5 h-4 bg-orange-400 rounded-full mt-4 ml-4"/></p></a>
             </div>
           </div>
         </div>
--- src/componets/Highlight.jsx
+++ src/componets/Highlight.jsx
@@ -1,42 +1,50 @@
 import React, { Component } from 'react';
 class Highlight extends Component {
-    render() { 
-        return (  
-            <div>
-                <section className="p-4 lg:p-8 dark:text-gray-100">
-	<div className="container mx-auto space-y-12">
-		<div className="flex flex-col overflow-hidden lg:shadow-neutral-content rounded-md shadow-sm lg:flex-row">
-			<img src="https://source.unsplash.com/640x480/?1" alt="" className="h-80 dark:bg-gray-500 aspect-video" />
-			<div className="flex flex-col justify-center flex-1 p-6 ">
-				<span className="text-xs uppercase text-neutral-content">Join, it's free</span>
-				<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
-				<p className="my-6 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
-				<button type="button" className="self-start text-neutral-content">Action</button>
+	render() {
+		return (
+			<div>
+				<section className="p-4 lg:p-8 dark:text-gray-100">
+						<h1 className="text-3xl font-semibold text-center text-gray-800 capitalize lg:text-4xl dark:text-white">
+							Highlights
+						</h1>
+						<div className="flex justify-center mx-auto mt-6 mb-10">
+							<span className="inline-block w-40 h-1 bg-neutral-content rounded-full" />
+							<span className="inline-block w-3 h-1 mx-1 bg-neutral-content rounded-full" />
+							<span className="inline-block w-1 h-1 bg-neutral-content rounded-full" />
+						</div>
+						<div className="container mx-auto space-y-24">
+							<div className="flex flex-col overflow-hidden  lg:flex-row">
+								<img src="https://source.unsplash.com/640x480/?1" alt="" className="h-80 dark:bg-gray-500 aspect-video" />
+								<div className="flex flex-col justify-center flex-1 p-6 ">
+									<span className="text-xs uppercase text-neutral-content">Join, it's free</span>
+									<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
+									<p className="my-6 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
+									<button type="button" className="self-start text-neutral-content">Action</button>
+								</div>
+							</div>
+							<div className="flex flex-col overflow-hidden lg:flex-row-reverse">
+								<img src="https://source.unsplash.com/640x480/?2" alt="" className="h-80 dark:bg-gray-500 aspect-video" />
+								<div className="flex flex-col justify-center flex-1 p-6 ">
+									<span className="text-xs uppercase text-neutral-content">Join, it's free</span>
+									<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
+									<p className="my-6 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
+									<button type="button" className="self-start text-neutral-content">Action</button>
+								</div>
+							</div>
+							<div className="flex flex-col overflow-hidden lg:flex-row">
+								<img src="https://source.unsplash.com/640x480/?3" alt="" className="h-80 dark:bg-gray-500 aspect-video" />
+								<div className="flex flex-col justify-center flex-1 p-6 ">
+									<span className="text-xs uppercase text-neutral-content">Join, it's free</span>
+									<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
+									<p className="my-6 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
+									<button type="button" className="self-start text-neutral-content">Action</button>
+								</div>
+							</div>
+						</div>
+				</section>
 			</div>
-		</div>
-		<div className="flex flex-col overflow-hidden lg:shadow-neutral-content rounded-md shadow-sm lg:flex-row-reverse">
-			<img src="https://source.unsplash.com/640x480/?2" alt="" className="h-80 dark:bg-gray-500 aspect-video" />
-			<div className="flex flex-col justify-center flex-1 p-6 ">
-				<span className="text-xs uppercase text-neutral-content">Join, it's free</span>
-				<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
-				<p className="my-6 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
-				<button type="button" className="self-start text-neutral-content">Action</button>
-			</div>
-		</div>
-		<div className="flex flex-col overflow-hidden lg:shadow-neutral-content rounded-md shadow-sm lg:flex-row">
-			<img src="https://source.unsplash.com/640x480/?3" alt="" className="h-80 dark:bg-gray-500 aspect-video" />
-			<div className="flex flex-col justify-center flex-1 p-6 ">
-				<span className="text-xs uppercase text-neutral-content">Join, it's free</span>
-				<h3 className="text-3xl font-bold">We're not reinventing the wheel</h3>
-				<p className="my-6 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor aliquam possimus quas, error esse quos.</p>
-				<button type="button" className="self-start text-neutral-content">Action</button>
-			</div>
-		</div>
-	</div>
-</section>
-            </div>
-        );
-    }
+		);
+	}
 }
- 
+
 export default Highlight;
