eef4d47ca162fd72a815fca1a26f4d94e6ecad7b
Royal Babu <123royalbabu@gmail.com>
2022-12-17 20:13:33 +0530

    made event details responsive

--- src/componets/EventDetails.jsx
+++ src/componets/EventDetails.jsx
@@ -15,10 +15,10 @@ export class EventDetails extends Component {
 
   render() {
     return (
-      <div>
+      <div className="h-screen">
         <video
            width={window.innerWidth}
-            className="right-0 bottom-0 top-0 min-h-[100%] bg-base-300 min-w-[100%] absolute -z-20"
+            className="max-h-screen w-screen object-cover right-0 bottom-0 top-0 min-h-[100%] bg-base-300 min-w-[100%] absolute -z-20"
             loop
             autoPlay
             muted
@@ -26,22 +26,24 @@ export class EventDetails extends Component {
           <source src={videoplayback} type="video/mp4" />
         </video>
 
-      <div className="hero min-h-screen   flex">
-        <div className="h-full w-full bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 flex m-4 md:p-3 md:mx-16 md:my-4">
-          <div className="hero-content flex flex-col justify-center md:flex-row">
+      <div className="hero h-screen py-[2vh] flex">
+        <div className="h-full w-full justify-center flex m-4 md:p-3 md:mx-16 md:my-4">
+          <div className="hero-content flex flex-col justify-center md:flex-row border-gray-100 border rounded-md backdrop-blur-3xl bg-gray-300  bg-clip-padding backdrop-filter  bg-opacity-10">
             <img
               src={this.event.src}
-              className="max-w-sm rounded-lg shadow-2xl w-full max-h-[80vh] md:w-[50vw]"
+              className="rounded-lg shadow-2xl object-cover w-4/5 max-h-[80vh] md:w-[50vw] xl:max-w-[30vw]"
               alt={this.event.event}
             />
-            <div className="h-full md:w-1/2 text-primary-focus">
+            <div className="h-full md:w-1/2 text-primary-focus flex flex-col justify-center">
               <h1 className="text-5xl font-bold text-neutral-content">{this.event.name}</h1>
               <h1 className="text-1xl font-bold text-primary-focus">{this.event.date} | {this.event.time}</h1>
               <h1 className="text-1xl font-bold text-neutral-content">{this.event.venue} </h1>
               <div className="h-[60vh] overflow-y-auto overflow-x-hidden">
                 <p className="py-6" dangerouslySetInnerHTML={{__html:this.event.content}}/> 
               </div>
+              <div className="flex justify-start">
               <button className="btn btn-outline mt-4 text-neutral-content ">Register</button>
+              </div>
             </div>
           </div>
         </div>
