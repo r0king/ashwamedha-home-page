b6c0385ee7f8f447c22a15c2204b7a61f578385c
Sid9021 <sidharthkajith9021@gmail.com>
2022-12-18 21:35:59 +0530

    guests content added

--- src/componets/Hero.jsx
+++ src/componets/Hero.jsx
@@ -32,12 +32,7 @@ export default class Hero extends Component {
                   </svg>
 
 
-                  {/* <div>
-        <div className="container-glitch">
-          <div className="glitch" data-text="ASHWAMEDHA">ASHWAMEDHA</div>
-          <div className="glow">ASHWAMEDHA</div>
-        </div>
-      </div> */}
+                  
                   <h2 className="mb-2 text-3xl leading-relaxed md:text-2xl sm:text-xl md:leading-snug text1">Space : The Timeless Infinity
                   </h2>
                   <p className="mb-4 text-sm md:text-base text-gray-50 text2">Explore your favourite events and
@@ -46,16 +41,7 @@ export default class Hero extends Component {
                     Register Now</a>
 
                 </div>
-                {/*<div className="justify-center mt-12 mb-6 ml-10 pl-60 md:mb-0 md:mt-0 md:ml-12 lg:w-2/3 ">
-        <div className="flex flex-wrap content-center h-48">
-          <div>
-            <img className="hidden inline-block mt-28 xl:block" src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png" /></div>
-          <div>
-            <img className="inline-block p-8 mt-24 md:mt-0 md:p-0" src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png" /></div>
-          <div>
-            <img className="hidden inline-block mt-28 lg:block" src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png" /></div>
-        </div>
-    </div>*/}
+                
               </div>
             </div>
           </section>
