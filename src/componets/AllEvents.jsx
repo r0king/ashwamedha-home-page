06e0db0dbd7ca08519e86399c2969a540d2762ff
josephgeorgepadayatty <josephgeorgepadayatty@gmail.com>
2022-12-19 11:45:14 +0530

    removed more info in all events

--- src/componets/AllEvents.jsx
+++ src/componets/AllEvents.jsx
@@ -18,28 +18,22 @@ export default class AllEvents extends Component {
         slides.forEach((slide) => {
             slidesHtml.push(
                 <div index={this.state.slides.length} key={slide.id}>
-                    <div className="main flex flex-shrink relative sm:w-auto max-w-[75vw] sm:max-w-[20vw] md:max-w-[25vw] max-h-[450px] transition duration-500 hover:scale-110 ">
+                    <a href={process.env.PUBLIC_URL + "/#/" + slide.id} target="_self"><div className="main flex flex-shrink relative sm:w-auto max-w-[75vw] sm:max-w-[20vw] md:max-w-[25vw] max-h-[450px] transition duration-500 hover:scale-110 ">
                         <img
+                        
                             src={slide.src}
                             alt="black chair and white table"
                             className="object-cover object-center w-full "
                         />
+                        
                         <div className="absolute flex flex-col justify-between flex-shrink-0 w-full h-full p-6 pb-2 bg-black bg-opacity-40 ">
                             <h2 className="text-base font-bold leading-4 lg:text-xl lg:leading-5 text-primary">
                                 {slide.venue}
                             </h2>
                             <h4 className="text-base leading-4 lg:text-lg lg:leading-5 ">{""}</h4>
-                            <div className="flex justify-center opacity-100 work">
-                                <a className="button-style1" href={process.env.PUBLIC_URL + "/#/" + slide.id} target="_self">
-                                    <span className="d">
-                                        <span></span>
-                                    </span>
-                                    <span>More Info</span>
-                                </a>
-                            </div>
+                            
                         </div>
-                    </div>
-                    {console.log("did mount?")}
+                    </div></a>
                 </div>
             );
         });
