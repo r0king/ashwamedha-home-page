a02d2ca29a0d155a2de89df1f7f5d7549036d819
Royal Babu <123royalbabu@gmail.com>
2022-12-19 16:41:34 +0530

    add event update guest

--- src/componets/Guest.jsx
+++ src/componets/Guest.jsx
@@ -8,7 +8,7 @@ class GuestBlock extends Component {
     return (
       <div>
         <section>
-          <div className="h-[32rem] ">
+          <div className="">
             <div className="container px-6 py-10 mx-auto">
               <h1
                 id="guest"
@@ -29,7 +29,7 @@ class GuestBlock extends Component {
               </p>
             </div>
           </div>
-          <div className="container flex items-center justify-center px-6 py-16 mx-auto -mt-72 sm:-mt-80 ">
+          <div className="container flex items-center justify-center px-6 py-16 mx-auto -mt-20  ">
             <div className="mt-[4vh]  h-[60vh] w-[40vh] flex  flex-col items-center p-4 border sm:p-6 rounded-xl dark:border-gray-700 ">
               <img
                 className="object-cover h-[54vh] w-[40vh] rounded-xl aspect-square"
