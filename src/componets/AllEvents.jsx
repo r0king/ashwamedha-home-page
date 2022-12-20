8ac5128ba05602594ece9ccfecea439d279a0f91
Royal Babu <123royalbabu@gmail.com>
2022-12-20 19:59:40 +0530

    setup events page tabs

--- src/componets/AllEvents.jsx
+++ src/componets/AllEvents.jsx
@@ -9,7 +9,42 @@ export default class AllEvents extends Component {
       slides: [],
     };
     this.slides = eventData;
+  }
+  componentDidUpdate(prevProps) {
+    if (this.props !== prevProps) {
+      const slides = this.slides;
+      const props = this.props;
+      const work = [5, 6, 10];
+      const comp = [0, 1, 2, 3, 4, 7, 8];
+      const general = [15, 19, 20];
 
+      if (props.type === "work") {
+        this.filterSlides = work;
+      } else if (props.type === "comp") {
+        this.filterSlides = comp;
+      } else if (props.type === "general") {
+        this.filterSlides = general;
+      } else {
+        this.filterSlides = Array.from(Array(eventData.length).keys());
+      }
+      let slidesHtml = [];
+      this.filterSlides.forEach((id) => {
+        slidesHtml.push(
+          <div index={id} key={id}>
+            <a href={process.env.PUBLIC_URL + "/#/events/" + id} target="_self">
+              <div className="main flex flex-shrink relative sm:w-auto max-w-[75vw] sm:max-w-[20vw] md:max-w-[25vw] max-h-[450px] transition duration-500 hover:scale-110 ">
+                <img
+                  src={slides[id].src}
+                  alt="black chair and white table"
+                  className="object-cover object-center w-full "
+                />
+              </div>
+            </a>
+          </div>
+        );
+      });
+      this.setState({ slides: slidesHtml });
+    }
   }
   componentDidMount() {
     const slides = this.slides;
@@ -25,7 +60,7 @@ export default class AllEvents extends Component {
     } else if (props.type === "general") {
       this.filterSlides = general;
     } else {
-      this.filterSlides = Array.from(Array(eventData.length).keys())
+      this.filterSlides = Array.from(Array(eventData.length).keys());
     }
     let slidesHtml = [];
     this.filterSlides.forEach((id) => {
@@ -44,29 +79,51 @@ export default class AllEvents extends Component {
       );
     });
     this.setState({ slides: slidesHtml });
-
   }
   render() {
-    console.log(this.props)
     return (
       <div>
-        {/* <div className="tabs">
-          <a href="/#/events/workshops" className={(this.props.type === "work") ? "tab tab-bordered tab-active" : "tab tab-bordered"}>Tab 1</a>
-          <a href="/#/events/competitions" className={(this.props.type === "comp") ? "tab tab-bordered tab-active" : "tab tab-bordered"}>Tab 2</a>
-          <a href="/#/events/general" className={(this.props.type === "general") ? "tab tab-bordered tab-active" : "tab tab-bordered"}>Tab 3</a>
-        </div> */}
+        <div className="tabs fixed top-5 h-[5vh] z-10 flex w-screen justify-center">
+          <a href="/#" className={"tab text-2xl tab-bordered"}>
+            Home
+          </a>
+          <a
+            href="/#/events/workshops"
+            className={
+              this.props.type === "work"
+                ? "tab text-2xl tab-bordered tab-active"
+                : "tab text-2xl tab-bordered"
+            }
+          >
+            Work Shops
+          </a>
+          <a
+            href="/#/events/competitions"
+            className={
+              this.props.type === "comp"
+                ? "tab text-2xl tab-bordered tab-active"
+                : "tab text-2xl tab-bordered"
+            }
+          >
+            Competitions
+          </a>
+          <a
+            href="/#/events/general"
+            className={
+              this.props.type === "general"
+                ? "tab text-2xl tab-bordered tab-active"
+                : "tab text-2xl tab-bordered"
+            }
+          >
+            General
+          </a>
+        </div>
 
         <div className="relative flex items-center justify-center w-full">
           <div className="w-full h-full mx-auto mt-[5vh]">
-            <div className="pl-10 mb-8">
-              <a href={process.env.PUBLIC_URL + "/#"}>
-                <button type="button" className="px-4 py-2 bg-transparent border rounded shadow text-neutral-content border-neutral-content hover:bg-neutral-content hover:text-black hover:shadow-lg hover:border-transparent">Back to Home</button>
-              </a>
-            </div>
-
             <div
               id="slider"
-              className="flex flex-wrap items-center justify-center mx-5 lg:gap-8 md:gap-6 gap-14"
+              className="flex mt-10 flex-wrap items-center justify-center mx-5 lg:gap-8 md:gap-6 gap-14"
             >
               {this.state.slides}
             </div>
